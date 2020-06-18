e11474aabde72688ea688f30f727f8b5803771e7
Igor Srdoc <igor@srdoc.si>
2020-06-18 18:10:37 +0200

    Add two users

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from smartninja_sql.sqlite import SQLiteDatabase
 
 db = SQLiteDatabase("hiking.sqlite")
 
-db.execute("CREATE TABLE User(name text, age integer);")
+db.execute("""
+    CREATE TABLE IF NOT EXISTS User(
+        id integer primary key autoincrement,
+        name text,
+        age integer
+    );
+""")
 
-db.print_tables()
+db.pretty_print("""
+    SELECT * 
+    FROM User;
+""")
+
+# db.print_tables(verbose=True)
